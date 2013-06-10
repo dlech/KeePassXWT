e19acfbcdc649cbaa196ed1b4de38f470c52d3da
David Lechner <david@lechnology.com>
2013-06-10 01:46:49 -0500

    Simplify toolkit calls.

--- KeePassXWT/Program.cs
+++ KeePassXWT/Program.cs
@@ -9,7 +9,7 @@ namespace KeePassXWT
 		// single apartment thread needed for windows backend
 		[STAThreadAttribute()]
 		static void Main(string[] args) {
-			string toolkit;
+			ToolkitType toolkitType;
 
 #if FORCE_GTK
 			switch (PlatformID.Unix) {
@@ -17,22 +17,19 @@ namespace KeePassXWT
 			switch (Environment.OSVersion.Platform) {
 #endif
 				case PlatformID.Win32NT:
-					var wpfAssm = Assembly.LoadFrom("Xwt.WPF.dll");
-					toolkit = string.Join(", ", "Xwt.WPFBackend.WPFEngine", wpfAssm.FullName);
+					toolkitType = ToolkitType.Wpf;
 					break;
 				case PlatformID.Unix:
-					var gtkAssm = Assembly.LoadFrom("Xwt.Gtk.dll");
-					toolkit = string.Join(", ", "Xwt.GtkBackend.GtkEngine", gtkAssm.FullName);
+					toolkitType = ToolkitType.Gtk;
 					break;
 				case PlatformID.MacOSX:
-					var cocoaAssm = Assembly.LoadFrom("Xwt.Mac.dll");
-					toolkit = string.Join(", ", "Xwt.Mac.MacEngine", cocoaAssm.FullName);
+					toolkitType = ToolkitType.Cocoa;
 					break;
 				default:
 					Console.WriteLine("Unsupported Platform");
 					return;
 			}
-			Application.Initialize (toolkit);
+			Application.Initialize (toolkitType);
 			using (var mainWindow = new MainWindow ()) {
 				mainWindow.Show ();
 				Application.Run ();
