1b1d6e0d6148c4c350d0ce304cfc3aea97f60e83
David Lechner <david@lechnology.com>
2013-06-26 20:09:09 -0500

    Create separate project for running Gtk App

---
+++ KeePassXwtGtk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xwt;
+using System.Reflection;
+using System.IO;
+
+namespace KeePassXWT
+{
+	public class Program
+	{
+		// single apartment thread needed for windows backend
+		[STAThreadAttribute ()]
+		static void Main (string[] args)
+		{
+			Application.Initialize (ToolkitType.Gtk);
+			using (var mainWindow = new MainWindow ()) {
+				mainWindow.Show ();
+				Application.Run ();
+			}
+		}
+	}
+}
+
